5e3e402146ad5c039ab23187cc3081b497a27aa4
artembondarchuck <artembondarchuck@yandex.by>
2021-04-21 12:33:31 +0300

    Initial

---
+++ lab6/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using lab6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace lab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            return View(db.Cars.ToList());
+        }
+
+        public IActionResult SearchByModel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchByModel(Car car)
+        {
+            Car car1 = db.Cars.FirstOrDefault(c => c.Model == car.Model);
+            return View(car1);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Car car)
+        {
+            db.Cars.Add(new Car { Model = car.Model, Price = car.Price });
+            db.SaveChanges();
+            return View();
+        }
+
+        public IActionResult SearchByPrice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchByPrice(CarManager carManager)
+        {
+            carManager.Cars = db.Cars.Where(c => c.Price <= carManager.Price).ToList();
+            return View(carManager);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ lab6/Models/Car.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab6.Models
+{
+    public class Car
+    {
+
+        private long id;
+        private string model;
+        private int price;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
